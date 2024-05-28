2e90eaa1146c4e791536f2462a62417f86c3fc93
yplog <yalinpala@protonmail.com>
2024-05-28 21:16:28 +0300

    add flag options

--- cmd/peerkat/main.go
+++ cmd/peerkat/main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
 	"github.com/yplog/peerkat/pkg/node"
 	"os"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	relayAddr := flag.String("relay", "", "Relay address")
+	peerAddr := flag.String("peer", "", "Peer address (optional)")
+	mode := flag.String("mode", "", "Mode (chat, file-transfer)")
 
-	fmt.Print("Enter relay address: ")
-	relayAddr, _ := reader.ReadString('\n')
-	relayAddr = relayAddr[:len(relayAddr)-1]
+	flag.Parse()
 
-	fmt.Print("Enter peer address: ")
-	peerAddr, _ := reader.ReadString('\n')
-	peerAddr = peerAddr[:len(peerAddr)-1]
+	if *relayAddr == "" || *mode == "" {
+		fmt.Println("Please provide relay address and mode as flags")
+		os.Exit(1)
+	}
+
+	if *mode != "chat" && *mode != "file-transfer" {
+		fmt.Println("Mode must be either 'chat' or 'file-transfer'")
+		os.Exit(1)
+	}
+
+	fmt.Print("Relay address: ", *relayAddr, "\n")
+	fmt.Print("Mode: ", *mode, "\n")
+
+	if *peerAddr != "" {
+		fmt.Print("Peer address: ", *peerAddr, "\n")
+	}
+	fmt.Println("Starting peerkat node...")
 
 	fmt.Print("Relay address: ", relayAddr, "\n")
 	fmt.Println("Starting peerkat node...")
 
-	peerNode := node.New(relayAddr, peerAddr)
+	peerNode := node.New(*relayAddr, *peerAddr)
 
 	peerNode.Start()
 }
